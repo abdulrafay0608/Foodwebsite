f877d6e0409477ff4f1b2f4dead66893fc93c2ff
abdulrafay0608 <121513581+abdulrafay0608@users.noreply.github.com>
2023-01-31 04:24:10 -0800

    Food Website

---
+++ Java Script/index.js
@@ -0,0 +1,14 @@
+//var percentage = prompt ('enter your percentage')
+
+if(percentage >= 50){
+    console.log('Pass')
+}else{
+    console.log('Fail')
+} 
+
+function rafayKhan(num){
+    console.log(num*num)
+}
+    
+rafayKhan(10)
+
